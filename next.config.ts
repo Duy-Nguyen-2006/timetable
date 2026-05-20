d32f66f5765ff4a475107e76d62d1566acafb82c
Duy <toan9ctranphu@gmail.com>
2026-05-20 23:40:17 +0700

    chore: update release build config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   outputFileTracingExcludes: {
-    "/*": ["./release/**/*", "./dist/**/*"],
+    "/*": ["./release/**/*", "./dist/**/*", "./.venv/**/*"],
   },
   /* config options here */
   typescript: {
